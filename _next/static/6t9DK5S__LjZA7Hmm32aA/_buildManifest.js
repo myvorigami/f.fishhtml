11bc9bbc928e6f472566241fa1a897c1e2061017
F F <f@f.fish>
2022-08-15 20:48:08 +0200

    v2

---
+++ _next/static/6t9DK5S__LjZA7Hmm32aA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-c9a7cfa07ff90bb4.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
